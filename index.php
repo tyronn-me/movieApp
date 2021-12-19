aa2deb5be8a99a77233a20abfd29b3df8bbb2f2e
Tyron <mail@tyronn.me>
2021-12-18 17:05:05 -0800

    update push

--- index.php
+++ index.php
@@ -8,9 +8,11 @@
   <meta name="author" content="Tyron">
   <meta name="description" content="Movie showtimes, reviews and more all in one place!">
   <meta name="keywords" content="movies, reviews, showtimes, movie, film, cinema">
-  <link href="https://fonts.googleapis.com/css?family=Muli:900|Open+Sans:400,800" rel="stylesheet">
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;900&display=swap" rel="stylesheet">
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.min.css" type="text/css">
-  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
   <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
   <link rel="stylesheet" href="css/styles.css" type="text/css">
   </head>
@@ -19,9 +21,7 @@
 	  <div id="featureMovie"></div>  
   
   <!-- requirements -->
-  <script src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
-  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
   <script src="https://unpkg.com/react@16/umd/react.development.js" crossorigin></script>
   <script src="https://unpkg.com/react-dom@16/umd/react-dom.development.js" crossorigin></script>
   <script src="https://unpkg.com/babel-standalone@6.26.0/babel.min.js"></script>
--- js/scripts.js
+++ js/scripts.js
@@ -6,6 +6,7 @@ class Init extends React.Component {
 	
 	constructor(props) {
         super(props);
+		const thi_s = this;
 		const configURL = "https://api.themoviedb.org/3/configuration?api_key=cb8075e06e3457700a64f46720e566ec";
 		const apiURL = "https://api.themoviedb.org/3/movie/popular?api_key=cb8075e06e3457700a64f46720e566ec&language=en-US&page=1";
 		
@@ -26,15 +27,19 @@ class Init extends React.Component {
 						"Date" : data.results[i].release_date
 					});
 				}
-				console.log(movieArr)
+				console.log(movieArr);
+
+				thi_s.state = { 
+					title : movieArr[0]["Title"], 
+					overview : movieArr[0]["Overview"],
+					backdrop : movieArr[0]["Backdrop"],
+					vote : movieArr[0]["Vote"],
+					date : movieArr[0]["Date"]
+				};
 			});
 		}).catch(function(err){
 			console.log("Fetch error: " + err);
 		});
-		
-		
-		// this.state = { targetURL : targetURL, thetitle : title, overview : overview, backdrop : backdrop, vote_average : vote_average, releateDate : moveDateFormate.toString() };	
-		
 		
     }
     
@@ -52,7 +57,21 @@ class Init extends React.Component {
 		
 		return (
 			<div className="container-fluid" id="mainContainer">
-
+				<nav className="navbar fixed-top navbar-expand-lg navbar-dark">
+					<div className="container-fluid">
+						<a className="navbar-brand" href="#">Movie<strong>Chill</strong></a>
+						<button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+						<span className="navbar-toggler-icon"></span>
+						</button>
+						<div className="collapse navbar-collapse" id="navbarSupportedContent">
+						<ul className="navbar-nav me-auto mb-2 mb-lg-0">
+						</ul>
+						<form className="d-flex" id="navForm">
+							<input className="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
+						</form>
+						</div>
+					</div>
+				</nav>
 			</div>
 		);
 		
